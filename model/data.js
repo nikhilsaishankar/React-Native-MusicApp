1b837c05b6a260afb348ac5bb02dfe6c353e275b
nikhilsaishankar <nikhilsaishankar@gmail.com>
2023-01-03 19:41:32 +0530

    Music

---
+++ model/data.js
@@ -0,0 +1,39 @@
+const songs = [
+  {
+    id: 1,
+    title: '19th Floor',
+    artist: 'Bobby Richards',
+    artwork: require('../assets/img/img1.jpg'),
+    url: 'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3',
+  },
+  {
+    id: 2,
+    title: 'Awful',
+    artist: 'josh pan',
+    artwork: require('../assets/img/img2.jpg'),
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+  },
+  {
+    id: 3,
+    title: 'Something is Going On',
+    artist: 'Godmode',
+    artwork: require('../assets/img/img3.jpg'),
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-17.mp3',
+  },
+  {
+    id: 4,
+    title: 'Book The Rental Wit It',
+    artist: 'RAGE',
+    artwork: require('../assets/img/img4.jpg'),
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-15.mp3',
+  },
+  {
+    id: 5,
+    title: 'Crimson Fly',
+    artist: 'Huma-Huma',
+    artwork: require('../assets/img/img5.jpg'),
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-14.mp3',
+  },
+];
+
+export default songs;
